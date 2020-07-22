2ade2e63bdc232ee9bf23b76fef35117c437a738
Unknown <martin.plaas+github@gmail.com>
2020-07-22 14:34:03 +0200

    add recipe-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
+
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    RecipesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
